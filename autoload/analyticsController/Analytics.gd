8d5fc4734f898b0cc1697d0141a00c4a89d7abb9
loop-nop <86272232+loop-nop@users.noreply.github.com>
2026-07-22 16:44:27 +0000

    start without AnalyticsSecrets

--- autoload/analyticsController/Analytics.gd
+++ autoload/analyticsController/Analytics.gd
@@ -3,10 +3,14 @@ extends Node
 var gameAnalytics;
 
 func _ready():
-	if(Engine.has_singleton("GameAnalytics")):
+	if( Engine.has_singleton("GameAnalytics")):
 		gameAnalytics = Engine.get_singleton("GameAnalytics")
-		gameAnalytics.init(AnalyticsSecrets.GAME_KEY, AnalyticsSecrets.SECRET_KEY)
 		gameAnalytics.setEnabledInfoLog(true);
+		
+		if (Engine.has_singleton("AnalyticsSecrets")):
+			var sicrets = Engine.get_singleton("AnalyticsSecrets")
+			gameAnalytics.init(sicrets.GAME_KEY, sicrets.SECRET_KEY)
+
 
 func sendDebugRequest(number: int):
 	var optArgs = {
