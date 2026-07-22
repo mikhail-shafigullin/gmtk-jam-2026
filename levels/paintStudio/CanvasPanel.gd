09429ec0a62e77711d89d29ee35c380be034417f
loop-nop <86272232+loop-nop@users.noreply.github.com>
2026-07-22 23:59:04 +0000

    canvas painter undo/redo

--- levels/paintStudio/CanvasPanel.gd
+++ levels/paintStudio/CanvasPanel.gd
@@ -7,30 +7,27 @@ extends Panel
 const IMG_SIZE := Vector2i(128, 128)
 const IMG_FORMAT := DrawableTexture2D.DRAWABLE_FORMAT_RGBA8
 
-var stroke_stack: Array[PaintStroke]
+var preview := DrawableTexture2D.new()
+var canvas := DrawableTexture2D.new()
 
-var preview: DrawableTexture2D
-var canvas: DrawableTexture2D
+var preview_sprite := Sprite2D.new()
+var canvas_sprite := Sprite2D.new()
 
-var preview_sprite: Sprite2D
-var canvas_sprite: Sprite2D
+var undo_position: int = 0
+var undo_stack: Array[PackedByteArray] = []
 
 func _ready() -> void:
 	_setup_sprites()
 	_on_resized()
 
-	pass
+
 
 func _setup_sprites():
 	#textures
-	preview = DrawableTexture2D.new()
 	preview.setup(IMG_SIZE.x, IMG_SIZE.y, IMG_FORMAT, Color.TRANSPARENT, false)
-	canvas = DrawableTexture2D.new()
 	canvas.setup(IMG_SIZE.x, IMG_SIZE.y, IMG_FORMAT, bg_color, false)
 
 	#sprites
-	preview_sprite = Sprite2D.new()
-	canvas_sprite = Sprite2D.new()
 	preview_sprite.name = "PreviewSprite"
 	canvas_sprite.name = "CanvasSprite"
 	preview_sprite.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
@@ -40,7 +37,11 @@ func _setup_sprites():
 	preview_sprite.texture = preview
 	canvas_sprite.texture = canvas
 
+	undo_position = 0
+	undo_stack.push_back(get_canvas_data())
+
 	resized.connect(_on_resized)
+
 
 func _on_resized():
 	if preview and canvas:
@@ -60,13 +61,21 @@ func _gui_input(event: InputEvent) -> void:
 				debug_brush.on_start(canvas, pos, brush_size, brush_color)
 			else:
 				debug_brush.on_end(canvas, pos, brush_size, brush_color)
-
+				commit()
+	
 	elif event is InputEventMouseMotion:
-		print_debug(event.position, event.relative)
+		# print_debug(event.position, event.relative)
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			var to := Vector2i(_pos_to_texture(event.position))
 			var from := Vector2i(_pos_to_texture(event.position - event.relative))
 			debug_brush.paint(canvas, from, to, brush_size, brush_color)
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed():
+		if event.is_action("paint_undo"):
+			undo()
+		elif event.is_action("paint_redo"):
+			redo()
 
 
 func _process(delta: float) -> void:
@@ -78,3 +87,43 @@ func _process(delta: float) -> void:
 
 func _pos_to_texture(pos: Vector2) -> Vector2:
 	return pos * (canvas.get_size() / size)
+
+
+func get_canvas_data() -> PackedByteArray:
+	var img := canvas.get_image()
+	return img.get_data()
+
+func set_canvas_data(new_data: PackedByteArray) -> void:
+	canvas.setup(IMG_SIZE.x, IMG_SIZE.y, IMG_FORMAT, bg_color, false)
+
+	var i := Image.create_from_data(IMG_SIZE.x, IMG_SIZE.y, false, Image.FORMAT_RGBA8, new_data)
+	var t := ImageTexture.create_from_image(i)
+	canvas.blit_rect(Rect2i(Vector2i.ZERO, IMG_SIZE), t)
+
+func commit() -> void:
+	if (undo_position < undo_stack.size() - 1):
+		undo_stack.resize(undo_position + 1)
+
+	undo_stack.push_back(get_canvas_data())
+	undo_position += 1
+
+func undo() -> void:
+	undo_position = clampi(undo_position-1, 0, undo_stack.size()-1)
+	set_canvas_data(undo_stack[undo_position])
+
+func redo() -> void:
+	undo_position = clampi(undo_position+1, 0, undo_stack.size()-1)
+	set_canvas_data(undo_stack[undo_position])
+
+func _on_undo_button_pressed() -> void:
+	undo()
+	pass # Replace with function body.
+
+
+func _on_redo_button_pressed() -> void:
+	redo()
+	pass # Replace with function body.
+
+
+func _on_color_picker_button_color_changed(color: Color) -> void:
+	brush_color = color
